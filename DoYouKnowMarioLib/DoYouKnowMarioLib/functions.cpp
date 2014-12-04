5863c1442d692fb4aeb287c997f9c50962d5c718
lemamon <demmarinho@gmail.com>
2014-12-04 11:43:33 -0400

    add files functions

--- DoYouKnowMarioLib/DoYouKnowMarioLib/functions.cpp
+++ DoYouKnowMarioLib/DoYouKnowMarioLib/functions.cpp
@@ -2,40 +2,87 @@
 #include <string>
 #include <cstdio>
 #include <ctime>
+#include <fstream>
 
 using namespace std;
 
-//função busca a proxiam ocorrecia na string passada. Tem como ponto de partida o parametro (int position)
+string openReadFile(string fl)
+{
+	string line, line2;
+	ifstream arq;
+	arq.open(fl);
+	while(!arq.eof())
+	{
+		getline(arq,line);
+		line2 += line;
+	}
+	arq.close();
+	return line2
+}
+
+void writeInEndOfFile(string fl, string content)
+{
+	string line, line2;
+	ifstream arq;
+	arq.open(fl, fstream::app);
+	arq<<content;
+	arq.close();
+}
+
+void writeInFile(string fl, string content)
+{
+	string line, line2;
+	ifstream arq;
+	arq.open(fl);
+	arq<<content;
+	arq.close();
+}
+
+int renameFile(string fl)
+{
+
+}
+
+int deleteFile(string fl)
+{
+	if( remove( fl ) != 0 )
+    	perror( "Error deleting file" );
+  	else
+    	puts( "File successfully deleted" );
+  	return 0;
+}
+//funÃ§Ã£o busca a proxiam ocorrecia na string passada. Tem como ponto de partida o parametro (int position)
 int findNext(string line, string search, int position)
 {
-	int pos = 0, pos1 = 0;
+	int pos=0, pos1=0;
 	for (int i = 0; i < position; ++i)
-	{
+	{   	
 		pos1 = line.find(search, pos);
-		pos = pos1 + 1;
+		pos = pos1+1;
 	}
 	return pos1;
 }
 
-//captura a data atual do sistema em formarto DD-MM-YYYY.HH:mm:ss
-const string currentDateTime()
-{
-	time_t     now = time(0);
-	struct tm  tstruct;
-	char       buf[80];
-	tstruct = *localtime(&now);
-	// Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
-	// for more information about date/time format
-	strftime(buf, sizeof(buf), "%d-%m-%Y  %X", &tstruct);
 
-	return buf;
+//captura a data atual do sistema em formarto DD-MM-YYYY.HH:mm:ss
+const string currentDateTime() 
+{
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%d-%m-%Y  %X", &tstruct);
+
+    return buf;
 }
 
 //retorna um numero float a partir de uma string
 float stringToFloat(string num) {
 	float n;
 	numero =
-		numero = atof(numeroString.c_str());
+	numero = atof(numeroString.c_str());
 	return numero;
 }
 
@@ -48,32 +95,32 @@ int stringToInt(string num) {
 
 //popular vetores
 void initVector(int be[], int bd[]) {
-	for (int i = 0; i<31; i++) {
-		be[i] = 0;
-		bd[i] = 0;
-	}
+     for (int i=0;i<31;i++) {
+         be[i] = 0;
+         bd[i] = 0;
+     }
 }
 
-//lipar uma string de pontuação e acentos
+//lipar uma string de pontuaÃ§Ã£o e acentos
 string clearText(string word){
 	char x;
 	int len = word.length();
-	for (int i = len; i >= 0; i--){
-		x = word[i];
-		if (isdigit(x) ||
-			(x == ',') ||
-			(x == '?') ||
-			(x == '\"') ||
-			(x == ')') ||
-			(x == '(') ||
-			(x == '+') ||
-			(x == '=') ||
-			(x == '_') ||
-			(x == '-') ||
-			(x == ':') ||
-			(x == ';') ||
-			(x == '!') ||
-			(x == '.'))
+	for(int i=len; i>=0; i--){
+        x = word[i];
+        if(isdigit(x) ||
+           (x == ',') ||
+           (x == '?') ||
+           (x == '\"') ||
+           (x == ')') ||
+           (x == '(') ||
+           (x == '+') ||
+           (x == '=') ||
+           (x == '_') ||
+           (x == '-') ||
+           (x == ':') ||
+           (x == ';') ||
+           (x == '!') ||
+           (x == '.'))
 			word.erase(i, 1);
 	}
 	return palavra;
